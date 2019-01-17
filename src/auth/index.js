22d6906b0bc1ee2e6437728e415e0df9af60ecbd
Mihelle Torres <lic.nancy.torres@gmail.com>
2019-01-17 14:32:16 -0500

    :nail_care: Linter happiness

--- src/auth/index.js
+++ src/auth/index.js
@@ -7,14 +7,10 @@ const settings = require('../settings');
  * Get the Github token from the auth file
  * @param  {String} [path=''] The path for the auth.json file
  * @return {String} The github token
- * @throws If the file is not present or can be read as json
  */
 async function getToken(jsonPath = '') {
   let auth = {};
-  let authPath = jsonPath;
-  if (!jsonPath) {
-    authPath = settings.authPath;
-  }
+  const authPath = jsonPath || settings.authPath;
   const authFile = path.resolve(authPath);
 
   try {
