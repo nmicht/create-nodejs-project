0917df6f5187828b747d84a8e16a04345ae39935
Mihelle Torres <lic.nancy.torres@gmail.com>
2019-02-18 18:03:23 -0500

    Unit test for process promisified

---
+++ test/unit-tests/utils/process.test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+
+const should = chai.should();
+
+const utils = require('../../../src/utils');
+
+describe('Utils for processes', () => {
+
+  it('execp returns a promise', async () => {
+    const p = utils.process.execp('ls')
+    return (p instanceof Promise).should.be.true;
+  });
+
+  it('spawn returns a promise', async () => {
+    const p = utils.process.spawnp('ls')
+    return (p instanceof Promise).should.be.true;
+  });
+});
